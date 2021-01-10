d6e818cd460553b67d68845af0d4c668e0b72fee
gabrielamandrade <u335782064@byui.edu>
2021-01-10 02:25:30 -0300

    initial commit

--- javascript.js
+++ javascript.js
@@ -1,3 +1,3 @@
 
-let x = new Date(document.lastModified) 
-document.document.getElementById("demo").innerHTML = x;
+let updated = new Date(document.lastModified) 
+document.getElementById("output").innerHTML = updated;
