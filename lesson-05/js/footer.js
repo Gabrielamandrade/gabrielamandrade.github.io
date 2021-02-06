5028dc54332f51d2c5bbbdbf66ff7358f925de1f
gabrielamandrade <u335782064@byui.edu>
2021-02-06 01:38:23 -0300

    initial commit

--- lesson-05/js/footer.js
+++ lesson-05/js/footer.js
@@ -1,1 +1,8 @@
-document.getElementById("output").innerHTML = new Date();
+
+let date = new Date(document.lastModified);
+let days = ["Sunday", "Monday","Tuesday", "Wednessday", "Thrusday", "Friday", "Saturday"];
+const months = ["January", "February", "March", "April", "May", "June", "July", "August", "Septiember", "Octuber", "November", "December"];
+
+let dayMain = days[date.getDay()]+", "+date.getDate() + " " + months[date.getMonth()] + " " + date.getFullYear();
+
+document.getElementById("output").innerHTML = dayMain;
