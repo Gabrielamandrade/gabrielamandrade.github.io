40f2c3231eca6657b0522215964e0131efe36167
gabrielamandrade <u335782064@byui.edu>
2021-02-07 03:44:49 -0300

    initial commit

--- lesson-05/js/header.js
+++
@@ -1,4 +0,0 @@
-let d= new Date(document.lastModified);
-let day = ["Sunday", "Monday","Tuesday", "Wednessday", "Thrusday", "Friday", "Saturday"];
-
-document.getElementById("out").innerHTML = Main;
