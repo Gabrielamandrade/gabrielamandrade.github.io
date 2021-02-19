ae444cfe40b14397804d300c269c58b5e41a99db
gabrielamandrade <u335782064@byui.edu>
2021-02-19 00:58:32 -0300

    initial commit

--- lesson-05/js/header.js
+++ lesson-05/js/header.js
@@ -1,14 +1,21 @@
+/*absolute
 function main() {
     var d = parseInt(readLine(), 10);
     let days = ["Sunday", "Monday","Tuesday", "Wednessday", "Thrusday", "Friday", "Saturday"];
     var day = 0;
     while (d >= 0) {
-        d -= 7;
-        day ++;
+        d -= 6;
+        days ++; 
         if (d > 0) {
             d += 2;
-        } else {
+        } else {   
             break;
         }
     } console.log(day);
 }
+if (days == 5)
+return "Saturday = Preston Pancakes in the Park!  9:00 a.m. Saturday at the city park pavilion." 
+*/
+
+let days = ["Sunday", "Monday","Tuesday", "Wednessday", "Thrusday", "Friday", "Saturday"];
+    
