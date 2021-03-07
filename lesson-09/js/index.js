b62794fb3e8f9e1192794d924d3f96315c465d20
gabrielamandrade <u335782064@byui.edu>
2021-03-07 03:33:46 -0300

    initial commit

--- lesson-09/js/index.js
+++ lesson-09/js/index.js
@@ -7,7 +7,9 @@ fetch(requestURL)
     .then(function (jsonObject) {
         const towns = jsonObject['towns'];
         const cards = document.querySelector(".cards");
+        
         for (let i = 0; i < towns.length; i++) {
+            if (towns[i].name == "Fish Haven"|| towns[i].name == "Preston" || towns[i].name == "Soda Springs") {
             let card = document.createElement("section");
             let h2 = document.createElement("h2");
             let pimg = document.createElement("img");
@@ -22,7 +24,7 @@ fetch(requestURL)
         p3.textContent = `Current Population: ${towns[i].currentPopulation}`;
         p4.textContent = `Average Rain Fall: ${towns[i].averageRainfall}`;
         p5.textContent = `Events: ${towns[i].events}`;         
-        pimg.setAttribute('src', towns[i].photo);
+        pimg.setAttribute('src', "image/" + towns[i].photo);
         pimg.setAttribute("alt", `${towns[i].name}`);
          card.appendChild(h2);
          card.appendChild(pimg);
@@ -32,6 +34,7 @@ fetch(requestURL)
          card.appendChild(p4);
          card.appendChild(p5);
          document.querySelector("div.cards").appendChild(card);
+            }
         }
         });
         //------------------------------------------------------------------------------------------------------------
